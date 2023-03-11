de55b9fa618c8c08e96edff01c5567b75389f383
HIDANBOT <6736895-HIDANBOT@users.noreply.replit.com>
2023-03-11 16:08:32 +0000

    test

--- cogs/Music.py
+++ cogs/Music.py
@@ -4,11 +4,11 @@ import discord
 import youtube_dl
 
 from discord.ext import commands
+
 discord.opus.load_opus("./libopus.so.0.8.0")
 
 # Suppress noise about console usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
-
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
@@ -21,12 +21,11 @@ ytdl_format_options = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address':
+    '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
-ffmpeg_options = {
-    'options': '-vn'
-}
+ffmpeg_options = {'options': '-vn'}
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
@@ -43,14 +42,16 @@ class YTDLSource(discord.PCMVolumeTransformer):
     @classmethod
     async def from_url(cls, url, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        data = await loop.run_in_executor(
+            None, lambda: ytdl.extract_info(url, download=not stream))
 
         if 'entries' in data:
             # take first item from a playlist
             data = data['entries'][0]
 
         filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options),
+                   data=data)
 
 
 class Music(commands.Cog):
@@ -71,7 +72,9 @@ class Music(commands.Cog):
         """Plays a file from the local filesystem"""
 
         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
-        ctx.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
+        ctx.voice_client.play(source,
+                              after=lambda e: print(f'Player error: {e}')
+                              if e else None)
 
         await ctx.send(f'Now playing: {query}')
 
@@ -81,7 +84,9 @@ class Music(commands.Cog):
 
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+            ctx.voice_client.play(player,
+                                  after=lambda e: print(f'Player error: {e}')
+                                  if e else None)
 
         await ctx.send(f'Now playing: {player.title}')
 
@@ -90,8 +95,12 @@ class Music(commands.Cog):
         """Streams from a url (same as yt, but doesn't predownload)"""
 
         async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+            player = await YTDLSource.from_url(url,
+                                               loop=self.bot.loop,
+                                               stream=True)
+            ctx.voice_client.play(player,
+                                  after=lambda e: print(f'Player error: {e}')
+                                  if e else None)
 
         await ctx.send(f'Now playing: {player.title}')
 
@@ -120,9 +129,11 @@ class Music(commands.Cog):
                 await ctx.author.voice.channel.connect()
             else:
                 await ctx.send("You are not connected to a voice channel.")
-                raise commands.CommandError("Author not connected to a voice channel.")
+                raise commands.CommandError(
+                    "Author not connected to a voice channel.")
         elif ctx.voice_client.is_playing():
             ctx.voice_client.stop()
 
+
 def setup(bot):
-    	bot.add_cog(Music(bot))
+    bot.add_cog(Music(bot))
